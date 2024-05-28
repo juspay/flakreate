51d7bb390b08cf159621eb8aa215dbd380e8d805
Sridhar Ratnakumar <srid@srid.ca>
2024-05-28 18:23:29 -0400

    chore: Use `inquire` crate
    And add TODOs for next steps.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use inquire::Text;
 
 #[derive(Parser, Debug)]
 #[clap(author = "Sridhar Ratnakumar", version, about)]
@@ -7,16 +8,24 @@ struct Args {
     /// whether to be verbose
     #[arg(short = 'v')]
     verbose: bool,
-
-    /// an optional name to greet
-    #[arg()]
-    name: Option<String>,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     if args.verbose {
         println!("DEBUG {args:?}");
     }
-    println!("Hello {}!", args.name.unwrap_or("world".to_string()));
+    println!("Welcome to flakreate! Let's create your flake template:");
+    let registry = Text::new("Template registry")
+        .with_help_message("Flake that contains a registry of templates to choose from")
+        .with_placeholder("Flake URL reference")
+        .with_default("github:flake-parts/templates")
+        .prompt()?;
+    println!("Using {}!", registry);
+
+    // TODO Read flake-parts/templates and eval it to JSON, then Rust types
+    // TODO Prompt to select a template to use
+    // TODO Prompt for the parameter values of the template
+    // TODO Run `nix flake init ...`, followed by the param patches
+    Ok(())
 }
