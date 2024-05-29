484d259c999272c369783e7d03092d656b584fec
Sridhar Ratnakumar <srid@srid.ca>
2024-05-28 20:41:20 -0400

    refactor:

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,25 @@ use std::collections::BTreeMap;
 
 use clap::Parser;
 use inquire::{Select, Text};
-use nix_rs::{command::NixCmd, flake::url::FlakeUrl};
+use nix_rs::{
+    command::{NixCmd, NixCmdError},
+    flake::url::FlakeUrl,
+};
 use serde::{Deserialize, Serialize};
+use tokio::sync::OnceCell;
+
+static NIXCMD: OnceCell<NixCmd> = OnceCell::const_new();
+
+pub async fn nixcmd() -> &'static NixCmd {
+    NIXCMD
+        .get_or_init(|| async {
+            NixCmd {
+                refresh: true.into(),
+                ..NixCmd::with_flakes().await.unwrap()
+            }
+        })
+        .await
+}
 
 #[derive(Parser, Debug)]
 #[clap(author = "Sridhar Ratnakumar", version, about)]
@@ -29,6 +46,30 @@ struct Param {
     exec: String,
 }
 
+impl Template {
+    pub async fn fetch_flake_templates(
+        url: &FlakeUrl,
+    ) -> Result<BTreeMap<String, Self>, NixCmdError> {
+        nix_rs::flake::eval::nix_eval_attr_json::<BTreeMap<String, Template>>(
+            nixcmd().await,
+            &url,
+            false,
+        )
+        .await
+    }
+}
+
+impl Param {
+    pub fn prompt_value(&self) -> anyhow::Result<String> {
+        let value = Text::new("Value")
+            .with_help_message(&self.help)
+            .with_placeholder(self.placeholder.as_deref().unwrap_or(""))
+            .with_default(self.default.as_deref().unwrap_or(""))
+            .prompt()?;
+        Ok(value)
+    }
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
@@ -43,16 +84,11 @@ async fn main() -> anyhow::Result<()> {
         .prompt()?;
     println!("Using {}!", registry);
 
-    let cmd = NixCmd {
-        refresh: true.into(),
-        ..NixCmd::default()
-    };
     let url: FlakeUrl = format!("{}#templates", registry).into();
 
     // Read flake-parts/templates and eval it to JSON, then Rust types
-    let templates =
-        nix_rs::flake::eval::nix_eval_attr_json::<BTreeMap<String, Template>>(&cmd, &url, false)
-            .await?;
+    let templates = Template::fetch_flake_templates(&url).await?;
+    // TODO: avoid duplicates (aliases)
     let names = templates.keys().collect::<Vec<_>>();
 
     // Let the user pick the template
@@ -66,16 +102,7 @@ async fn main() -> anyhow::Result<()> {
         .unwrap()
         .params
         .iter()
-        .map(|(name, param)| {
-            let default = param.default.as_deref().unwrap_or("");
-            let placeholder = param.placeholder.as_deref().unwrap_or("");
-            let value = Text::new(name)
-                .with_help_message(&param.help)
-                .with_placeholder(placeholder)
-                .with_default(default)
-                .prompt()?;
-            Ok((name.clone(), value))
-        })
+        .map(|(name, param)| Ok((name.clone(), param.prompt_value()?)))
         .collect::<anyhow::Result<BTreeMap<String, String>>>()?;
 
     // println!("Templates: {:#?}", templates);
